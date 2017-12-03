e3559362258d60c853c44be62e7d26fac023db21
vyshaal <vyshaal@ccs.neu.edu>
2017-12-03 00:05:24 -0500

    pet server

---
+++ petserver/app.js
@@ -0,0 +1,92 @@
+/**
+ * Created by vyshaalnarayanam on 12/2/17.
+ */
+
+module.exports = function (app,db) {
+
+    app.post('/api/pet',createPet);
+    app.get('/api/pet', findPetByNameAndBreed);
+    app.get('/api/pets', findPets);
+    app.get('/api/pet/:id',findPetById);
+
+    // Add table if not exists
+    db.serialize(function(){
+        db.run("CREATE TABLE IF NOT EXISTS pets (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, type TEXT, breed TEXT, location TEXT, latitude TEXT, longitude TEXT)");
+    });
+
+// API:1 Get all pets
+    function findPets(req,res) {
+        var pets = [];
+        res.contentType('application/json');
+        db.all("SELECT id as id, name as name, type as type, breed as breed FROM pets",
+            function (err, rows) {
+                if (err) {
+                    res.send(createError(err));
+                } else {
+                    res.send(createResponse(rows));
+                }
+            });
+    }
+
+// API:2 Get a pet profile
+    function findPetById(req,res) {
+        var id = req.params.id;
+        console.log(id);
+        res.contentType('application/json');
+        db.all("SELECT * FROM pets WHERE id = ?", Number(id), function (err, row) {
+            if (err) {
+                res.send(createError(err));
+            } else {
+                console.log(row);
+                res.send(createResponse(row));
+            }
+        });
+    }
+
+// API:3 Register a pet
+    function createPet(req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.contentType('application/json');
+        var name = req.body.name;
+        var type = req.body.type;
+        var breed = req.body.breed;
+        var location = req.body.location;
+        var latitude = req.body.latitude;
+        var longitude = req.body.longitude;
+        // Add new pet
+        db.run("INSERT INTO pets (name, type, breed, location, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?)", name, type, breed, location, latitude, longitude, function (err, row) {
+            if (err) {
+                res.send(createError(err));
+            } else {
+                console.log(row);
+                res.send(createResponse(row));
+            }
+        });
+    }
+
+    // API:4 Get a pet by name and breed
+    function findPetByNameAndBreed(req,res) {
+        var name = req.query.name;
+        var breed = req.query.breed;
+        console.log(name,breed);
+        res.contentType('application/json');
+        db.all("SELECT * FROM pets WHERE name = ? AND breed = ?", name, breed, function (err, row) {
+            if (err) {
+                res.send(createError(err));
+            } else {
+                console.log(row);
+                res.send(createResponse(row));
+            }
+        });
+    }
+
+// Helper functions
+    function createResponse(data) {
+        return {'success': true, 'data': data}
+    }
+
+    function createError(err) {
+        return {'success': false, 'error': err}
+    }
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+// Create Express instance
+var express = require('express');
+var app = express();
+
+// Add Body-Parser to parse post parameters
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Create Database
+var sqlite3 = require('sqlite3').verbose();
+var path = require('path');
+var dbPath = path.resolve(__dirname, 'pets.db') // Get DB path
+var db = new sqlite3.Database(dbPath)           // Create database if not exists 
+
+require("./petserver/app.js")(app,db);
+
+// Start listening 
+app.listen(process.env.PORT || 3000, function () {
+    console.log('App listening on port ' + (process.env.PORT || 3000));
+});
